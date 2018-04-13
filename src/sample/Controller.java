e061502d12fed599cfb3469bcb21c358d662595c
Bartosz Maleta <bartosz.maleta@gmail.com>
2018-04-13 20:49:53 +0200

    initialize method added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
-public class Controller {
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    Button buttonClickMe;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        buttonClickMe.setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent event)
+            {
+                System.out.println("Welcome world");
+            }
+        });
+    }
 }
